a296d218d10994adf83cf974822e3a9ef47d6060
chari00 <macharitocruzuk@gmail.com>
2023-08-11 13:18:21 +0100

    modify index.html and navbar component.

--- src/components/navs/Navbar.js
+++ src/components/navs/Navbar.js
@@ -4,13 +4,13 @@ export default function Navbar() {
 
   return (
     < div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg bg-dark navbar-light " >
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav ">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>

37f2677a5b75fdd21893c78d4a1a81219d0934c1
chari00 <macharitocruzuk@gmail.com>
2023-08-11 13:12:11 +0100

    modify game.js and game.css file.

---
+++ src/components/game/Game.js
@@ -0,0 +1,13 @@
+import './game.css'
+
+export default function Game() {
+
+
+    return (
+      < div className="game-title">
+      <h1 > Tic Tac Toe Game</h1>
+  
+        
+      </div>
+    );
+  }

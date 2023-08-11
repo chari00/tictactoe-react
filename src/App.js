8c87c7c1e8616003b54853b238fc8baad788aecd
chari00 <macharitocruzuk@gmail.com>
2023-08-11 13:20:26 +0100

    import Game, Square, Board components to App.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from './components/navs/Navbar';
-import './App.css';
+import Game from './components/game/Game';
+import Square from './components/square/Square';
+import Board from './components/board/Board';
 
 function App() {
   return (
     < >
     <Navbar/>
+    <Game/>
+    <Square/>
+    <Board/>
 
       
     </>
